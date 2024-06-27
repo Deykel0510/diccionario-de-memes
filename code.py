eb624e27c067dd73517cf912d9f3bba31d1d4740
Deykel0510 <kjrm1903@gmail.com>
2024-06-26 18:32:46 -0600

    Create code.py
    Diccionario basico de phython donde escribes en mayuscul para saber el significado de algunas frases de internet

---
+++ code.py
@@ -0,0 +1,31 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "I always comback" : "una frase dicha por wilian afton de la saga Five nihts at fredys que significa yo siempre regreso",
+            "pro" : "significa que alguien o algo es epico",
+            "Epiardo" : "significa que alguien es pro , epico o crak",
+            "jijijaja" : " risa o burla que salio de clash Royal" ,
+            "fachero"  : "significa que es epico o bonito usado varias veses con sarcasmo o para hacer un chiste      "
+            }
+            
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Esta palabra nisiquiera yo la conozco")meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "I always comback" : "una frase dicha por wilian afton de la saga Five nihts at fredys que significa yo siempre regreso",
+            "pro" : "significa que alguien o algo es epico",
+            "Epiardo" : "significa que alguien es pro , epico o crak",
+            "jijijaja" : " risa o burla que salio de clash Royal" ,
+            "fachero"  : "significa que es epico o bonito usado varias veses con sarcasmo o para hacer un chiste      "
+            }
+            
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Esta palabra nisiquiera yo la conozco")
